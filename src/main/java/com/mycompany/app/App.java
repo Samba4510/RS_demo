652fa8c7f3783fc96c99d459cc336cf51389d90b
Samba4510 <143478388+Samba4510@users.noreply.github.com>
2023-08-30 09:43:57 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "welcome to DIET" );
+        System.out.println( "welcome to the DIET" );
     }
 }
