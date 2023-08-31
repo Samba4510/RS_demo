fff95dfac80d3d1fcc56a88c1404bbcf4631092b
Samba4510 <143478388+Samba4510@users.noreply.github.com>
2023-08-31 15:15:55 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "welcome to the DIET" );
+        System.out.println( "This is project K" );
     }
 }
